bc225658564bc4f620104766d0541eca1efa7660
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2019-05-30 21:57:18 -0400

    added lalalala for testing

--- query.php
+++ query.php
@@ -16,6 +16,8 @@ list ( $sqlin, $param1 ) = $DB->get_in_or_equal ( $param);
 $param2 = ["profesoreditor", "profesornoeditor"];
 $params = array_merge($param1, $param2);
 var_dump($params);
+echo "<br>";
+var_dump("lalalala");
 
 $query = "SELECT 
                 CONCAT(c.id,'-',u.id) as superid,
